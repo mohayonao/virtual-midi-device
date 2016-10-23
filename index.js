d5c135bcc0a89803201930c86be7a91b46ea1916
mohayonao <mohayonao@gmail.com>
2016-10-23 12:18:54 +0900

    update LEDColorDefs

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-module.exports = {};
+module.exports = {
+  LEDColorDefs: require("./components/LEDColorDefs").default,
+};
--- src/components/LEDColorDefs.js
+++ src/components/LEDColorDefs.js
@@ -1,29 +1,16 @@
-import React, { Component, PropTypes } from "react";
+import React, { PureComponent, PropTypes } from "react";
 import Color from "color";
 
-export default class LEDColorDefs extends Component {
+export default class LEDColorDefs extends PureComponent {
   static propTypes = {
     colors: PropTypes.arrayOf(PropTypes.string).isRequired,
   };
 
-  constructor(...args) {
-    super(...args);
-
-    this._colors = this.props.colors.map((color, i) => {
+  render() {
+    const elems = this.props.colors.map((color, i) => {
+      const id = `led${ i }`;
       const color1 = Color(color).mix(Color("#fafaf9")).darken(0.25).hexString();
       const color2 = i !== 0 ? Color(color1).lighten(0.5).hexString() : color1;
-
-      return [ color1, color2 ];
-    });
-  }
-
-  shouldComponentUpdate() {
-    return false;
-  }
-
-  render() {
-    const elems = this._colors.map(([ color1, color2 ], i) => {
-      const id = `led${ i }`;
 
       return (
         <radialGradient key={ i } gradientUnits="objectBoundingBox" id={ id } spreadMethod="pad">
